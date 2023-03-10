0de31757a3b6b67259ae757f16e097346db8f73b
Lanpaiva <alanpaiva@mail.io>
2023-03-10 11:20:08 -0300

    fix: comments added

--- pkg/events/interfaces.go
+++ pkg/events/interfaces.go
@@ -2,20 +2,23 @@ package events
 
 import "time"
 
+//EVENTOS: carrega dados
 type EventInterface interface {
 	GetName() string
 	GetDateName() time.Time
-	GetPayload() interface{}
+	GetPayload() interface{} //INTERFACE VAZIA POIS QUALQUER COISA PODE IMPLEMENTAR O PAYLOAD
 }
 
+//OPERAÇÕES QUANDO EVENTO É CHAMADO
 type EventHandlerInterface interface {
-	Handler(event EventInterface)
+	Handler(event EventInterface) //EXECUTA A OPERAÇÃO, POR ISSO PRECISA DO EventInterface
 }
 
+//GERENCIADOR
 type EventDispatcherInterface interface {
-	Register(eventName, string, handler EventHandlerInterface) error
-	Dispatch(event EventInterface) error
-	Remove(eventName string, handler EventHandlerInterface) error
-	Has(eventName string, handler EventHandlerInterface) bool
-	Clear() error
+	Register(eventName, string, handler EventHandlerInterface) error //QUANDO ESTE ESTE EVENTO FOR EXECUTADO, EXECUTA O Handler
+	Dispatch(event EventInterface) error                             //FAZ COM QUE O EVENTO ACONTEÇA E QUE OS EVENTOS SEJAM EXECUTADOS
+	Remove(eventName string, handler EventHandlerInterface) error    // REMOVER O EVENTO DA LISTA
+	Has(eventName string, handler EventHandlerInterface) bool        //VERICAA SE O TEM UM EENTNAME COM ESTE NOME
+	Clear() error                                                    //LIMPA O EVENTDISPACHER
 }
