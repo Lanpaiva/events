65ff7aa84297d263d6801202c4aa5413aff729ef
Lanpaiva <alanpaiva@mail.io>
2023-03-13 16:27:38 -0300

    chore: mock test dispatch added

--- pkg/events/event.dispatcher.go
+++ pkg/events/event.dispatcher.go
@@ -14,6 +14,15 @@ func NewEventDispatch() *EventDispatcher {
 	}
 }
 
+func (ev *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ev.handlers[event.GetName()]; ok {
+		for _, handler := range handlers {
+			handler.Handler(event)
+		}
+	}
+	return nil
+}
+
 func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
 	if _, ok := ed.handlers[eventName]; ok {
 		for _, h := range ed.handlers[eventName] {
