36bc21f980ac81d27253902acdf64174013e53f1
Lanpaiva <alanpaiva@mail.io>
2023-03-15 11:51:36 -0300

    feat: consumer added to main project

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -16,7 +16,7 @@ func OpenChannel() (*amqp.Channel, error) {
 
 func Consume(ch *amqp.Channel, out chan<- amqp.Delivery) error {
 	msgs, err := ch.Consume(
-		"minha fila",
+		"minhafila",
 		"go-consumer",
 		false,
 		false,
